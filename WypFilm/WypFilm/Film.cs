d79f2ec60fc604b37468d0039c896a2bd85c6d75
GanGabriel <95363614+GanGabriel@users.noreply.github.com>
2025-01-15 00:23:01 +0100

    Utworzenie klas: film, klient, wypozyczalnia
    Utworzenie klas: film, klient, wypozyczalnia
    Dodanie pliku gitignore

---
+++ WypFilm/WypFilm/Film.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wypozyczalnia
+{
+    internal class Film
+    {
+        public int id, rok_pr;
+        public string tytul, gatunek;
+        public bool dostepnosc;
+        public void zmianaDost()
+        {
+            dostepnosc = !dostepnosc;
+        }
+
+        public Film(int id, int rok_pr, string tytul, string gatunek, bool dostepnosc)
+        {
+            this.id = id;
+            this.rok_pr = rok_pr;
+            this.tytul = tytul;
+            this.gatunek = gatunek;
+            this.dostepnosc = dostepnosc;
+        }
+    }
+}
---
+++ WypFilm/WypFilm/Klient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Wypozyczalnia
+{
+    internal class Klient
+    {
+        public int id, max_wyp;
+        public int akt_wyp = 0;
+        public string imie, nazwisko, mail, telefon;
+        
+
+
+        public Klient(int id, string imie, string nazwisko, string mail, string telefon, int max_wyp)
+        {
+            this.id = id;
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.mail = mail;
+            this.telefon = telefon;
+            this.max_wyp = max_wyp;
+        }
+    }
+}
