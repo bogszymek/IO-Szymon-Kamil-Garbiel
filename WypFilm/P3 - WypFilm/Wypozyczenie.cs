1339bc2be287e44bee4352acb9c31cc90bbc7046
bogszymek <78368392+bogszymek@users.noreply.github.com>
2025-01-15 00:55:58 +0100

    Zmiana Wypozyczenie.cs
    Dodanie funkcji RejestracjaWypożyczenia(), RejestracjaZwrotu();

--- WypFilm/P3 - WypFilm/Wypozyczenie.cs
+++ WypFilm/P3 - WypFilm/Wypozyczenie.cs
@@ -21,6 +21,51 @@ namespace Wypozyczalnia
             DataZwrotu = null;
         }
 
+        public static string RejestracjaWypozyczenia(int idKlient, int idFilm)
+        {
+            Film film = Wypozyczalnia.filmy[idFilm - 1];
+            Klient klient = Wypozyczalnia.klienci[idKlient - 1];
+
+            if (klient.akt_wyp >= klient.max_wyp)
+            {
+                return "Klient osiągnął limit wypożyczeń.";
+
+            }
+
+            if (!film.dostepnosc)
+            {
+                return "Film nie jest dostępny.";
+
+            }
+
+            film.dostepnosc = false;
+            klient.akt_wyp++;
+            Wypozyczenie noweWypozyczenie = new Wypozyczenie(film, klient);
+            Wypozyczalnia.wypozyczenia.Add(noweWypozyczenie);
+            return $"\nPomyślnie wypożyczono film '{film.tytul}'";
+        }
+        public static string RejestracjaZwrotu(int idKlient, int idZw)
+        {
+            Klient klient = Wypozyczalnia.klienci[idKlient - 1];
+            var wypozyczeniaKlienta = Wypozyczalnia.wypozyczenia.Where(w => w.klient.id == idKlient && w.DataZwrotu == null).ToList();
+
+            Wypozyczenie wypoDoZwrotu = wypozyczeniaKlienta.FirstOrDefault(w => w.film.id == idZw);
+            if (wypoDoZwrotu != null)
+            {
+                Film film = Wypozyczalnia.filmy[idZw - 1];
+                if (film != null)
+                {
+                    film.dostepnosc = true;
+                }
+                wypoDoZwrotu.DataZwrotu = DateTime.Now;
+
+                klient.akt_wyp--;
+
+                return ($"Film '{film.tytul}' został zwrócony.");
+            }
+            else return "\nNie wypożyczono tego filmu!";
+        }
     }
+
 
 }
