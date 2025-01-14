733d1ba217b1e9d6764b64ff645a44a682c54cb9
GanGabriel <95363614+GanGabriel@users.noreply.github.com>
2025-01-15 00:26:45 +0100

    Uzupełnienie klasy: wypozyczalnia
    Dodanie metod: dodajFilm, dodajKlient

--- WypFilm/WypFilm/Wypozyczalnia.cs
+++ WypFilm/WypFilm/Wypozyczalnia.cs
@@ -9,7 +9,21 @@ namespace Wypozyczalnia
 {
     internal class Wypozyczalnia
     {
-       
-        
+        public static List<Klient> klienci = new List<Klient>();
+        public static List<Film> filmy = new List<Film>();
+        public static List<Wypozyczenie> wypozyczenia = new List<Wypozyczenie>();
+
+        public void dodajFilm(string tytul, int rok_pr, string gatunek)
+        {
+            Film nowy = new Film(filmy.Count + 1, rok_pr, tytul, gatunek, true);
+            filmy.Add(nowy);
+        }
+
+        public void dodajKlient(string imie, string nazwisko, string mail, string telefon)
+        {
+            Klient nowy = new Klient(klienci.Count + 1, imie, nazwisko, mail, telefon, 3);
+            klienci.Add(nowy);
+        }
+
     }
 }
